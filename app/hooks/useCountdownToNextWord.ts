57d73f9d450f34c0be77266001f9f4ca90f8c00a
maneaGauthier <manea.gauthier@student.junia.com>
2026-04-24 14:51:14 +0200

    fix arthaud

--- app/hooks/useCountdownToNextWord.ts
+++ app/hooks/useCountdownToNextWord.ts
@@ -1,24 +1,24 @@
 import { useEffect, useState } from "react";
 
 function useCountdownToNextWord() {
-  const now = new Date();
-  const midnight = new Date();
-  midnight.setHours(24, 0, 0, 0); // prochain minuit
-
-  const initialSeconds = Math.floor(
-    (midnight.getTime() - now.getTime()) / 1000,
-  );
-  const [seconds, setSeconds] = useState(initialSeconds);
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
-    if (seconds <= 0) return;
+    const calculateRemaining = () => {
+      const now = new Date();
+      const midnight = new Date();
+      midnight.setHours(24, 0, 0, 0); // prochain minuit
+      return Math.floor((midnight.getTime() - now.getTime()) / 1000);
+    };
+
+    setSeconds(calculateRemaining());
 
     const interval = setInterval(() => {
-      setSeconds((prev) => prev - 1);
+      setSeconds(calculateRemaining());
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [seconds]);
+  }, []);
 
   return seconds;
 }
